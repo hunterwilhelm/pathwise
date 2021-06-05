dcf2be7c9f0978e1e90c5781f53e45cf73714b6e
Hunter Wilhelm <yeskindofday@gmail.com>
2021-06-04 22:42:56 -0600

    Cookie init

---
+++ src/modules/data.utils.js
@@ -0,0 +1,13 @@
+class DataUtils {
+  static getRandomConnectionId() {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const length = 32;
+    let result = '';
+    for (let i = length; i > 0; --i) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+  }
+}
+
+module.exports = DataUtils;
