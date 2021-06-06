cf7fc7b0d950c0bcee5c616c068fa8b485186c18
Hunter Wilhelm <yeskindofday@gmail.com>
2021-06-05 23:53:35 -0600

    typescript webpack nodejs base

---
+++ webpack-back.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: {
+    app: ["./src/back/index.ts"]
+  },
+  output: {
+    path: path.resolve(__dirname, "./build/back"),
+    filename: "bundle.js"
+  },
+  externals: [nodeExternals()],
+}
